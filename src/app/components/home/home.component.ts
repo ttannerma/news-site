a56f4624e4a9b90dd04f9961a13f373406d5b275
Teemu Tannerma <teemu.tannerma@gmail.com>
2020-05-13 12:00:13 +0300

    Changed input fields to select fields when creating query

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -8,13 +8,37 @@ import { NewsService } from 'src/app/services/news.service';
   styleUrls: ['./home.component.css']
 })
 
-
 export class HomeComponent implements OnInit {
 
   sources: Object
   newsSource
+  isDataLoaded: boolean;
+
+  // List of possible languages
+  languages = {
+    ar: 'Arabic',
+    de: 'German',
+    en: 'English',
+    es: 'Spanish',
+    fr: 'French',
+    he: 'Hebrew',
+    it: 'Italian',
+    nl: 'Dutch',
+    no: 'Norwegian',
+    pt: 'Portuguese',
+    ru: 'Russian',
+    se: 'Swedish',
+    zh: 'Chinese'
+  }
+
+  queryObject = {
+    keyword: '',
+    sourceName: '',
+    language: ''
+  }
+
   constructor(private newsService : NewsService, private router: Router) {
-    
+    this.isDataLoaded = false
   }
 
   ngOnInit(): void { 
@@ -28,15 +52,33 @@ export class HomeComponent implements OnInit {
         this.newsSource = await this.newsService.getNewsSources()
         .toPromise()
         .then((resp) => {
-          console.log(resp['sources'])
           this.sources = resp['sources']
+          this.isDataLoaded = true;
           return resp['sources']
         });
     }
+    this.isDataLoaded = true;
     return this.sources;
   }
+
+  // event handler for sourcename pick
+  handleSourceNamePick(value) {
+    this.queryObject.sourceName = value
+  }
+
+  // event handler for language pick
+  handleLanguagePick(value) {
+    this.queryObject.language = value
+  }
+
+  // key up event handler for keyword
+  onKeywordInput(event) {
+    this.queryObject.keyword = event.target.value
+  }
+
+  // On submit handler
   onSubmit(userForm) {
-    this.newsService.setQueryData(userForm.value)
+    this.newsService.setQueryData(this.queryObject)
     this.router.navigateByUrl('/news')
   }
 }
